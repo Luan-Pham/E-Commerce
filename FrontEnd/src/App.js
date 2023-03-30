d7b709a2ba5822c2ddf165b0e60b637725c952e7
unknown <phamexb@gmail.com>
2023-03-29 17:49:44 -0700

    initial navbar

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,12 +1,15 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <>
       <Header />
-      <main>
-        <h1>Welcome to my shop!</h1>
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to my shop!</h1>
+        </Container>
       </main>
       <Footer />
     </>
