eae281e119fb8560a3d778777036b41460a3e8f4
unknown <phamexb@gmail.com>
2023-03-31 00:34:23 -0700

    fixed react router error, must import as router first and then routes/route

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,20 +1,26 @@
+import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
-}
+};
 
 export default App;
