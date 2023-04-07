0dd4c02c1e98f13bd067e8ece9be429fba85367e
unknown <phamexb@gmail.com>
2023-04-06 23:28:23 -0700

    configureStore

--- FrontEnd/src/store.js
+++ FrontEnd/src/store.js
@@ -1,17 +1,10 @@
-import { configureStore, combineReducers, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducer = combineReducers();
-
-const initialState = {};
-
-const middleware = [thunk];
-
-const store = configureStore(
-  reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware([...middleware]))
-);
+const store = configureStore({
+  reducer: {},
+  preloadedState: {},
+  middleware: [thunk],
+});
 
 export default store;
