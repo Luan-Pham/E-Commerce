8c5417016ef1d4989d47a70ba12fe1f3af85710e
unknown <phamexb@gmail.com>
2023-04-11 18:08:06 -0700

    cartScreen started

--- FrontEnd/src/screens/CartScreen.js
+++ FrontEnd/src/screens/CartScreen.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link, useParams, useNavigate } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import { addToCart } from '../actions/cartActions';
 
 const CartScreen = () => {
+  const navigate = useNavigate();
+  const params = useParams();
+
+  const productId = params.id;
+
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
   return <div>Cart</div>;
 };
 
--- FrontEnd/src/screens/ProductScreen.js
+++ FrontEnd/src/screens/ProductScreen.js
@@ -17,7 +17,7 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 
 const ProductScreen = () => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
   const params = useParams();
   const navigate = useNavigate();
 
