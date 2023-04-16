b6971216dcae97a7a27481cd56d7454fb95f0a04
unknown <phamexb@gmail.com>
2023-04-15 22:33:08 -0700

    cart add

--- FrontEnd/src/screens/CartScreen.js
+++ FrontEnd/src/screens/CartScreen.js
@@ -13,13 +13,21 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import { addToCart } from '../actions/cartActions';
 
-const CartScreen = () => {
+const CartScreen = (location) => {
   const navigate = useNavigate();
   const params = useParams();
 
   const productId = params.id;
 
   const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
 
   return <div>Cart</div>;
 };
